e2de73393acdaf70e5a56777d6376a0c1c57158e
ryuring <egashira@catchup.co.jp>
2025-07-16 21:06:32 +0900

    調整

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
 export { ApiClient } from "./basercms-js-sdk";
 export { getBlogPost, getBlogPosts, addBlogPost } from "./bc-blog-post";
 export type { BlogPost } from "./bc-blog-post";
